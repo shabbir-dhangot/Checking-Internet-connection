f68abf29dfb2cd3501a54f24fb1a306c058e94f0
Shabbir Dhangot <shabbir-dhangot@users.noreply.github.com>
2017-07-03 10:07:01 +0530

    Removed deprecated method.

--- ConnectionDetector.java
+++ ConnectionDetector.java
@@ -14,18 +14,20 @@ public class ConnectionDetector
  
     public boolean isConnectingToInternet()
     {
-        ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
-          if (connectivity != null) 
-          {
-              NetworkInfo[] info = connectivity.getAllNetworkInfo();
-              if (info != null) 
-                  for (int i = 0; i < info.length; i++) 
-                      if (info[i].getState() == NetworkInfo.State.CONNECTED)
-                      {
-                          return true;
-                      }
- 
-          }
-          return false;
+        ConnectivityManager cm = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (activeNetwork != null) { // connected to the internet
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
+                // connected to wifi
+                Log.p("Internet WIFI");
+                return true;
+            } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
+                // connected to the mobile provider's data plan
+                Log.p("Internet DATA");
+                return true;
+            }
+        }
+        // not connected to the internet
+        return false;
     }
 }
